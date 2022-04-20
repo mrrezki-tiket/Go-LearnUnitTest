87d49d6be19b9dc543d4e2455021fba9ff67d94a
Muhammad Rezki Ananda <muhammad.ananda@tiket.com>
2022-04-20 17:02:29 +0700

    Unit test : failed

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
-	result := HelloWord("Rezki")
+	result := HelloWord("Nando")
 	if result != "Hello Rezki" {
 		// unit test failed
-		panic("Result is not Hello Rezki ")
+		t.Error("Result must be `Hello Rezki")
 	}
+
+	fmt.Println("TestHelloWord done")
+}
+
+func TestHelloWorld2(t *testing.T) {
+	result := HelloWord("Ekki")
+	if result != "Hello Rezki" {
+		// unit test failed
+		t.Fatal("Result must be `Hello Rezki")
+	}
+
+	fmt.Println("TestHelloWord done")
 }
