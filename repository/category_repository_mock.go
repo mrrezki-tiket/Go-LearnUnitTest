20f3a84cf33db40c89abb1db6a987685a7577f96
Muhammad Rezki Ananda <muhammad.ananda@tiket.com>
2022-04-21 12:21:32 +0700

    Mock

---
+++ repository/category_repository_mock.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"belajar-golang-unit-test/entity"
+	"github.com/stretchr/testify/mock"
+)
+
+type CategoryRepositoryMock struct {
+	Mock mock.Mock
+}
+
+func (repository *CategoryRepositoryMock) FindById(id string) *entity.Category {
+	argument := repository.Mock.Called(id)
+	if argument.Get(0) == nil {
+		return nil
+	}
+	category := argument.Get(0).(entity.Category)
+	return &category
+}
